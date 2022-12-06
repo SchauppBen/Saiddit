602c92cab97c1f9e134ac2c96e24e49c113fa6fd
Tommy Moreno <tommy61002@gmail.com>
2022-12-06 10:14:13 -0500

    some controller files created

---
+++ java/src/main/java/com/techelevator/controller/PostController.java
@@ -0,0 +1,15 @@
+package com.techelevator.controller;
+
+import com.techelevator.dao.PostDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+
+    @Autowired
+    private PostDao dao;
+
+    
+
+}
---
+++ java/src/main/java/com/techelevator/dao/JdbcForumDao.java
@@ -0,0 +1,12 @@
+package com.techelevator.dao;
+
+import org.springframework.stereotype.Component;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+@Component
+public class JdbcForumDao implements ForumDao {
+
+
+
+}
