5367adf97ae04e19b0bf8beeddf9fd4d524db323
Thomas Hyatt <thomaschyatt42@gmail.com>
2022-12-07 15:45:25 -0500

    comming before pulling

--- vue/src/views/CreatePost.vue
+++ vue/src/views/CreatePost.vue
@@ -1,5 +1,10 @@
 <template>
-  <form action=""></form>
+    <form class="new-book-form" v-on:submit.prevent="saveBook">
+        <input class="title-input" type="text" placeholder="Title"/>
+        <input class="media-input" type="text" placeholder="Media Link"/>
+        <input class="text-input" type="text" placeholder="Text"/>
+        <button>Add Post</button>
+    </form>
 </template>
 
 <script>
