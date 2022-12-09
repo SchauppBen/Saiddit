28e6db55f5441525b32c1987ba704d66489224b4
Ben Schaupp <schauppben@gmail.com>
2022-12-09 13:35:15 -0500

    Methods in PostVotes

--- java/src/main/java/com/techelevator/dao/PostVoteDao.java
+++ java/src/main/java/com/techelevator/dao/PostVoteDao.java
@@ -2,9 +2,25 @@ package com.techelevator.dao;
 
 import com.techelevator.model.PostVote;
 
+import java.util.List;
+
 public interface PostVoteDao {
 
     void upVotePost(PostVote postVote);
 
     void downVotePost(PostVote postVote);
+
+    List<PostVote> getAllPostVotes();
+
+    List<PostVote> getPostVotesByUser(int userId);
+
+    List<PostVote> getPostVotesByPost(int postId);
+
+    List<PostVote> getUpVotesByUser(int userId);
+
+    List<PostVote> getDownVotesByUser(int userId);
+
+    List<PostVote> getUpVotesByPost(int postId);
+
+    List<PostVote> getDownVotesByPost(int postId);
 }
