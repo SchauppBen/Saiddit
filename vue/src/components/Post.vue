d62cae82482fb8cd1f92f8333e521cf954779f8d
Kevin Crawford <kpc22@pitt.edu>
2022-12-10 12:30:15 -0500

    added upvotes and downvotes

--- vue/src/components/Post.vue
+++ vue/src/components/Post.vue
@@ -1,32 +1,38 @@
 <template>
   <div id="post-container" class="posts">
-    <router-link
-      :to="{
-        name: 'post-details',
-        params: { forumName: post.forumName, postId: post.postId },
-      }"
-    >
-      <div id="postBox">
-        <h4>
-          <router-link
-            class="highlighted"
-            :to="{ name: 'forum-view', params: { forumName: post.forumName } }"
-            >[{{ post.forumName }}]</router-link
-          >
-        </h4>
+    <div id="postBox">
+      <h4>
+        <router-link
+          class="highlighted"
+          :to="{ name: 'forum-view', params: { forumName: post.forumName } }"
+          >[{{ post.forumName }}]</router-link
+        >
+      </h4>
+      <router-link
+        :to="{
+          name: 'post-details',
+          params: { forumName: post.forumName, postId: post.postId },
+        }"
+      >
         <h1>{{ post.title }}</h1>
-        <img :src="post.mediaLink" v-show="post.mediaLink" />
-        <h3>{{ post.text }}</h3>
-        <h2>
-          <router-link
-            class="highlighted"
-            :to="{ name: 'user-posts', params: { username: post.username } }"
-            >User: {{ post.username }}</router-link
-          >
-        </h2>
-        <h3>{{ post.datetime }}</h3>
+        <img id="post-image" :src="post.mediaLink" v-show="post.mediaLink" />
+        <h3>{{ post.text }}</h3></router-link
+      >
+      <h2>
+        <router-link
+          class="highlighted"
+          :to="{ name: 'user-posts', params: { username: post.username } }"
+          >User: {{ post.username }}</router-link
+        >
+      </h2>
+      <h3>{{ post.datetime }}</h3>
+      <div id="votes">
+        <img id="up-arrow" src="../assets/arrowup.png" />#of upvotes<img
+          id="down-arrow"
+          src="../assets/arrowdown.png"
+        />#of downvotes
       </div>
-    </router-link>
+    </div>
   </div>
 </template>
 
@@ -39,8 +45,18 @@ export default {
 </script>
 
 <style scoped>
-#postBox img {
+#post-image {
   width: 500px;
+}
+
+#votes img {
+  height: 20px;
+}
+#up-arrow:hover {
+  background-color: green;
+}
+#down-arrow:hover {
+  background-color: red;
 }
 
 #post-container {
