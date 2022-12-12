ca64a3ec2576cb31279f2f96d1a5aa9aec1a587e
Tommy Moreno <tommy61002@gmail.com>
2022-12-12 10:31:59 -0500

    fixed some styling and added vote buttons!

--- vue/src/main.js
+++ vue/src/main.js
@@ -5,10 +5,20 @@ import store from './store/index'
 import axios from 'axios'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faMagnifyingGlass, faUser} from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faUser, faCircleUp, faCircleDown, faCircleUser } from '@fortawesome/free-solid-svg-icons'
+import { faArrowAltCircleUp, faArrowAltCircleDown } from '@fortawesome/free-regular-svg-icons'
 
-library.add(faMagnifyingGlass, faUser)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+library.add(faMagnifyingGlass, faUser, faCircleUp, faArrowAltCircleUp, faArrowAltCircleDown, 
+  faCircleDown, faCircleUser)
+Vue.component("font-awesome-icon", FontAwesomeIcon, {
+  props: ["text"],
+  template: "#font-awesome-icon",
+  data() {
+    return {
+      isActive: true
+    }
+  }
+})
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
